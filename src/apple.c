197a36dbba43f3c8f212b2fa34a1ec151a631ce9
Tsunoda Kazuhiro <tsunoda.k.ae@m.titech.ne.jp>
2019-06-18 16:49:51 +0900

    a

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int w = 0;
+    for(int i = 0; i < n; i++) w =  w + ((A[i]+m-1) / m); 
+    
+    return w <= k ;
+    }
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +18,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+lb = 0;
+ ub = 1e9 + 1;
+ while(ub - lb > 1){
+  int m =  (ub + lb) / 2;
+if(p(m)) ub = m;
+else lb = m;
+  }
+   printf("%d\n",ub);
 
   return 0;
 }
--- src/array.c
+++ src/array.c
@@ -11,8 +11,13 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+ lb = -1;
+ ub = n;
+ while(ub - lb > 1){
+  int m =  (ub + lb) / 2;
+if(A[m] >= k) ub = m;
+else lb = m;
+  }
+   printf("%d\n",ub);
   return 0;
 }

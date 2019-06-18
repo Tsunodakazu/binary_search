8714d1bc69dc596ac7a860aa3c4eb4c28361f280
Tsunoda Kazuhiro <tsunoda.k.ae@m.titech.ne.jp>
2019-06-18 18:32:52 +0900

    a

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int w = 0;
+    for(int i = 0; i < n; i++)
+    { w =  w + (A[i] / m); 
+    }
+    return w < k ;
+    }
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,13 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+lb = 0;
+ ub = 1e9 + 1;
+ while(ub - lb > 1){
+  int m =  (ub + lb) / 2;
+if(p(m)) ub = m;
+else lb = m;
+  }
+   printf("%d\n",lb);
   return 0;
 }
